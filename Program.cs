60bb6344c21cb8416b45b799268a52aeb53a369d
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-10-29 16:47:53 +0200

    Updated-1.

--- Program.cs
+++ Program.cs
@@ -14,37 +14,43 @@ namespace Step_Homework_3
             первых 3 цифр равна сумме вторых трех цифр). Если пользователь ввел не
             шестизначное число – сообщение об ошибке.*/
 
-            /*Console.Write("Введите 1-е число: ");
-            double a1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Введите 2-е число: ");
-            double a2 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Введите 3-е число: ");
-            double a3 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Введите 4-е число: ");
-            double a4 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Введите 5-е число: ");
-            double a5 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Введите 6-е число: ");
-            double a6 = Convert.ToDouble(Console.ReadLine());
-            if(a1 + a2 + a3 == a4 + a5 + a6)
+            /*Console.Write("Введите 6-изначное число: ");
+            string X = Console.ReadLine();
+            if(Convert.ToInt32(X) > (Convert.ToInt32(X) % 1000000))
             {
-                Console.WriteLine("Число - счастливое!");
+                Console.WriteLine("Число - не 6-изначное (введено более 6 символов)");
             }
-            else if
-                (
-                Convert.ToInt32(a1) != Convert.ToDouble(a1) ||
-                Convert.ToInt32(a2) != Convert.ToDouble(a2) ||
-                Convert.ToInt32(a3) != Convert.ToDouble(a3) ||
-                Convert.ToInt32(a4) != Convert.ToDouble(a4) ||
-                Convert.ToInt32(a5) != Convert.ToDouble(a5) ||
-                Convert.ToInt32(a6) != Convert.ToDouble(a6)
-                )
+            else if (X.Length != 6)
             {
-                Console.WriteLine("Число - не целое!");
+                Console.WriteLine("Число - не 6-изначное (введено менее 6 символов)");
             }
             else
             {
-                Console.WriteLine("Число - не счастливое!");
+                Console.WriteLine("Число - 6-изначное");
+                int A1 = Convert.ToInt32(X) / 100000;
+                int A2 = Convert.ToInt32(X) / 10000 % 10;
+                int A3 = Convert.ToInt32(X) / 1000 % 10;
+                int A4 = Convert.ToInt32(X) % 1000 / 100;
+                int A5 = Convert.ToInt32(X) % 100 / 10;
+                int A6 = Convert.ToInt32(X) % 10;
+                Console.Write(A1 + " + ");
+                Console.Write(A2 + " + ");
+                Console.Write(A3);
+                int B1 = A1 + A2 + A3, B2 = A4 + A5 + A6;
+                if(B1 == B2)
+                {
+                    Console.Write(" = " + A4 + " + ");
+                    Console.Write(A5 + " + ");
+                    Console.WriteLine(A6);
+                    Console.WriteLine("Число - счастливое: " + B1 + " и " + B2 + "!");
+                }
+                else
+                {
+                    Console.Write(" != " + A4 + " + ");
+                    Console.Write(A5 + " + ");
+                    Console.WriteLine(A6);
+                    Console.WriteLine("Число - не счастливое: " + B1 + " и " + B2 + "!");
+                }
             }
             Console.ReadKey();*/
 
@@ -54,31 +60,54 @@ namespace Step_Homework_3
             3 и 4 цифры. Если пользователь вводит не четырехзначное
             число – вывести сообщение об ошибке.*/
 
-
-            /*Console.Write("Введите 1-е число: ");
-            double a1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Введите 2-е число: ");
-            double a2 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Введите 3-е число: ");
-            double a3 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Введите 4-е число: ");
-            double a4 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(a2);
-            Console.WriteLine(a1);
-            Console.WriteLine(a4);
-            Console.WriteLine(a3);*/
-            Console.Write("Введите число: ");
-            int a = Convert.ToInt32(Console.ReadLine());
-            if(a > a % 9999)
+            /*Console.Write("Введите 4-изначное число: ");
+            string X = Console.ReadLine();
+            if(Convert.ToInt32(X) > (Convert.ToInt32(X) % 10000))
             {
-                Console.WriteLine("Число - не 4-значное!");
+                Console.WriteLine("Число - не 4-изначное (введено более 4 символов)");
+            }
+            else if (X.Length != 4)
+            {
+                Console.WriteLine("Число - не 4-изначное (введено менее 4 символов)");
             }
             else
             {
-                Console.WriteLine(a);//
+                Console.WriteLine("Число - 4-изначное");
+                int A1 = Convert.ToInt32(X) / 1000 % 10;
+                int A2 = Convert.ToInt32(X) % 1000 / 100;
+                int A3 = Convert.ToInt32(X) % 100 / 10;
+                int A4 = Convert.ToInt32(X) % 10;
+                Console.Write(X + " | ");
+                Console.Write(A2);
+                Console.Write(A1);
+                Console.Write(A4);
+                Console.Write(A3);
             }
+            Console.ReadKey();*/
 
-            Console.ReadKey();
+
+            /*Задание 3: Пользователь вводит с клавиатуры 7 целых чисел.
+            Напишите программу, которая определяет максимальное из этих
+            7 чисел (Подсказка – решение должно быть простым).*/
+
+            /*Console.Write("Enter X1: ");
+            int A1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter X2: ");
+            int A2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter X3: ");
+            int A3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter X4: ");
+            int A4 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter X5: ");
+            int A5 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter X6: ");
+            int A6 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter X7: ");
+            int A7 = Convert.ToInt32(Console.ReadLine());
+            int[] A = { A1, A2, A3, A4, A5, A6, A7 };
+            int max_numb = A.Max();
+            Console.WriteLine("Самое большое число - " + max_numb);
+            Console.ReadKey();*/
         }
     }
 }
